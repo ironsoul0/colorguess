3f97909fd90ba64d44352e81520cd7fd031728d9
T1mka <timka2609@gmail.com>
2018-06-26 16:45:10 +0600

    second commit

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Message = (props) => {
+  const icon = (props.message[0] === 'W' ? <i className="fa fa-smile-wink" /> : <i className="fa fa-sad-tear" />);
+  const additionalContent = (props.message[0] === 'W' ? <p>You scored 10 right answers!</p> : <p>You lost all your lifes!</p>);
   return (
     <div className="Message">
+      <p className="Conclusion">
+        {icon}
+      </p>
       <h1>
         {props.message}
       </h1>
-      <button onClick={props.prepareGame}>
+      {additionalContent}
+      <button onClick={props.prepareGame} className="Again">
         Try again?
       </button>
     </div>
